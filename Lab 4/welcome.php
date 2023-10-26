348490e007e0a92cae4fc115108a37bbbbbf8063
Ossama Tammam <ossamatammam02@gmail.com>
2023-10-27 01:30:28 +0200

    changes

--- Lab 4/welcome.php
+++ Lab 4/welcome.php
@@ -4,12 +4,15 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css" />
   <title>Welcome</title>
 </head>
 
 <body>
-  <div class="welcome-message">
+  <div class="container">
     <?php
     // Check if the "name" query parameter is set
     if (isset($_GET['name'])) {
