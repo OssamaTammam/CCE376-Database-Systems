1271476a53bdabda42e9e773ae0347ff29bec777
Ossama Tammam <ossamatammam02@gmail.com>
2023-10-27 00:24:00 +0200

    حسبي الله و نعم الوكيل

--- Lab 4/loginForm.php
+++ Lab 4/loginForm.php
@@ -4,26 +4,44 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="css/style.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css" />
   <title>Login</title>
 </head>
 
 <body>
-  <form method="post" action="login.php" name="signin-form">
-    <div class="form-element">
-      <label for="email">Email</label>
-      <input type="email" name="email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}" required />
-    </div>
-    <div class="form-element">
-      <label for="password">Password</label>
-      <input type="password" name="password" required />
-    </div>
-    <button type="submit" name="login" value="login">Log In</button>
+  <div class="container">
+    <form method="post" action="login.php" onsubmit="return validateForm()" name="signin-form">
+      <div class="form-element">
+        <label for="email">Email</label>
+        <input type="email" name="email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}" required id="email" />
+      </div>
+      <div class="form-element">
+        <label for="password">Password</label>
+        <input type="password" name="password" required id="password" />
+      </div>
+      <button type="submit" name="login" value="login">Log In</button>
 
-  </form>
-  <a href="registerForm.php">
-    <button>Registration Form</button>
-  </a>
+    </form>
+    <a href="registerForm.php">
+      <button>Registration Form</button>
+    </a>
+  </div>
+  <script>
+    function validateForm() {
+      var email = document.getElementById("email").value;
+      var password = document.getElementById("password").value;
+
+      if (email === "" || password === "") {
+        alert("Email and Password cannot be empty!");
+        return false;
+      }
+
+      return true;
+    }
+  </script>
 </body>
 
 </html>
--- Lab 4/registerForm.php
+++ Lab 4/registerForm.php
@@ -4,26 +4,49 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="css/style.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css" />
   <title>Register</title>
 </head>
 
 <body>
-  <form method="post" action="register.php" name="signup-form">
-    <div class="form-element">
-      <label for="name">Name</label>
-      <input type="name" name="name" required />
-    </div>
-    <div class="form-element">
-      <label for="email">Email</label>
-      <input type="email" name="email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}" required />
-    </div>
-    <div class="form-element">
-      <label for="password">Password</label>
-      <input type="password" name="password" required />
-    </div>
-    <button type="submit" name="register" value="register">Register</button>
-  </form>
+  <div class="container">
+    <form method="post" action="register.php" onsubmit="return validateForm()" name="signup-form">
+      <div class="form-element">
+        <label for="name">Name</label>
+        <input type="name" name="name" required id="name" />
+      </div>
+      <div class="form-element">
+        <label for="email">Email</label>
+        <input type="email" name="email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}" required id="email" />
+      </div>
+      <div class="form-element">
+        <label for="password">Password</label>
+        <input type="password" name="password" required id="password" />
+      </div>
+      <button type="submit" name="register" value="register">Register</button>
+    </form>
+    <a href="loginForm.php">
+      <button>Login Form</button>
+    </a>
+  </div>
+  <script>
+    function validateForm() {
+      var name = document.getElementById("name").value;
+      var email = document.getElementById("email").value;
+      var password = document.getElementById("password").value;
+
+      if (name === "" || email === "" || password === "") {
+        alert("Please fill in all required fields.");
+        return false; // Prevent the form from submitting
+      }
+
+      return true; // Allow the form to submit if all required fields are filled
+    }
+  </script>
+
 </body>
 
 </html>
