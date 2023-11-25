337efba33ccf4ecf487e202f8eeb7b6ea6fb8220
Ossama Tammam <ossamatammam02@gmail.com>
2023-11-25 05:39:30 +0200

    done

--- Lab_7/DML.sql
+++ Lab_7/DML.sql
@@ -3,13 +3,12 @@ SELECT customer_name
 FROM customer
 WHERE customer_name LIKE 'Ma%';
 -- Question 2: Write an SQL query to retrieve count of items and total price of each order
-SELECT COUNT(item_id),
-    sum(quantity * unit_price)
-FROM (
-        order_item
-        NATURAL JOIN item
-    )
-GROUP BY order_id;
+SELECT order_id,
+    SUM(order_item.quantity) AS item_quantity,
+    SUM(order_item.quantity * item.unit_price) AS total_price
+FROM order_item
+    LEFT JOIN item ON order_item.item_id = item.item_id
+GROUP BY order_item.order_id;
 -- Question 3: Write an SQL query to retrieve order number for each order that had been shipped from warehouse in ‘Arica’
 SELECT order_id
 FROM(
